82c2125cc778d9c81d9877bc884653265f2f61d1
skittlecamp <81906603+skittlecamp@users.noreply.github.com>
2024-11-26 13:42:58 +0100

    Add files via upload

--- rainbowCursor.js
+++ rainbowCursor.js
@@ -4,7 +4,7 @@ var canvas = document.querySelector('canvas'),
 
 // Setting the width and height of the canvas
 canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.height = window.innerHeight*1;
 
 // Setting up the letters
 var letters = 'SKITTLECAMPSKITTLECAMPSKITTLECAMPSKITTLECAMPSKITTLECAMPSKITTLECAMPSKITTLECAMP';
